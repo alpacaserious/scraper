7c3cf821e6ff4b9368eb93a14f672b69f64b2cc0
AlpacaSerious <jerry8520@gmail.com>
2025-11-10 18:28:28 +0100

    impl downloading categories

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,54 @@ async fn main() {
     let url = std::env::args().nth(1).expect("URL should be provided");
     let client = Client::new();
 
-    download_gallery(&url, &client).await;
+    if url.contains("/thumbnails.php") {
+        download_album(&url, &client).await;
+    } else if url.contains("?cat=") {
+        download_category(&url, &client).await;
+    } else {
+        println!("Unsupported URL");
+    }
 }
 
-async fn download_gallery(url: &str, client: &Client) {
+async fn download_category(url: &str, client: &Client) {
+    let base_idx = url
+        .find("/index.php")
+        .expect("URL should contain '/index.php'");
+    let base_url = url.split_at(base_idx).0;
+
+    let albums = get_alb_links(url, client, 1).await;
+    println!("Found {} albums", albums.len());
+
+    for a in albums {
+        let link = format!("{base_url}/{a}");
+        download_album(&link, &client).await;
+    }
+}
+
+async fn get_alb_links(url: &str, client: &Client, page_idx: usize) -> Vec<String> {
+    let res = client.get(url).send().await.expect("GET request succesful");
+
+    let body = res.text().await.expect("get the response text");
+    let document = Html::parse_document(&body);
+    let alb_links = Selector::parse(".alblink > a").expect("parsed to find album links");
+
+    let next_page_links = match get_next_page(&document, page_idx) {
+        Some(n) => {
+            let next_url = format!("{url}&page={n}");
+            Box::pin(get_alb_links(&next_url, client, page_idx + 1)).await
+        }
+        None => vec![],
+    };
+
+    document
+        .select(&alb_links)
+        .filter_map(|l| l.attr("href"))
+        .map(|l| l.to_string())
+        .chain(next_page_links)
+        .collect()
+}
+
+async fn download_album(url: &str, client: &Client) {
     let base_idx = url
         .find("/thumbnails")
         .expect("URL should contain '/thumbnails'");
@@ -84,7 +128,7 @@ async fn get_links_from_url(url: &str, client: &Client, page_idx: usize) -> Vec<
 
     let next_page_links = match get_next_page(&document, page_idx) {
         Some(n) => {
-            let next_url = format!("{url}?page={n}");
+            let next_url = format!("{url}&page={n}");
             Box::pin(get_links_from_url(&next_url, client, page_idx + 1)).await
         }
         None => vec![],
@@ -95,7 +139,7 @@ async fn get_links_from_url(url: &str, client: &Client, page_idx: usize) -> Vec<
         .filter_map(|l| l.attr("src"))
         .map(|l| l.to_string())
         .chain(next_page_links)
-        .collect::<Vec<_>>()
+        .collect()
 }
 
 async fn get_image(url: &str, client: &Client, gallery: &str, idx: usize) {
